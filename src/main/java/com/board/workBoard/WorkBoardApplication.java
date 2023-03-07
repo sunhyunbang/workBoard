ad7e82d214a8e8997a2f86602ad55ed569f44f33
0189875 <0189875@naver.com>
2023-03-07 13:44:30 +0900

    요청 게시판 프로젝트 생성

---
+++ src/main/java/com/board/workBoard/WorkBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.workBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/board/workBoard/WorkBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.workBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
