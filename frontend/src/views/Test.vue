74aff62a3858125de4f7081d3f90e246070b92bf
jongseop <jsongeop@mediawill.com>
2023-03-10 14:35:48 +0900

    chore: spring boot + vue.js one build 추가

---
+++ frontend/src/views/Test.vue
@@ -0,0 +1,16 @@
+<template>
+  테스트입니다
+  <p>{{ msg }}</p>
+</template>
+<script>
+
+export default {
+  name: 'TesT',
+  components: {},
+  data() {
+    return {
+      msg: "TEST",
+    }
+  },
+}
+</script>
---
+++ frontend/vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  outputDir: "../src/main/resources/static", // build target
+  devServer: {
+    proxy: {
+      '/': { // 해당 값에 따라 8080으로 proxy 처리
+        target: 'http://localhost:8080',
+        changeOrigin: true // cross origin 허용
+      }
+    }
+  }
+})
